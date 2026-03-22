9175aa2921b46c92731f3fdc6d36bdf5f5643097
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-22 15:58:13 +0530

    apply cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cookie from "cookie-parser"
 import AuthRoute from "./routes/auth.route.js"
+import cors from "cors"
 const app = express();
+
 
 
 
 app.use(express.json());
 app.use(cookie());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}))
 
 
 app.use("/api/auth",AuthRoute);
